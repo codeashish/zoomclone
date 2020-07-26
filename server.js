77ba4f60ae57630673b965c8639a497cda10a452
Ashish Guleria <bhagwan@pop-os.localdomain>
2020-07-26 19:10:46 +0530

    Init Commit 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT||8080;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(port)
+console.log(process.env.PORT)
 server.listen(port, () => {
   console.log("Server is liten in port ");
 });
