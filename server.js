0bef1560282e7ccd8c6d73468c62ad6537cd90d6
Ashish Guleria <bhagwan@pop-os.localdomain>
2020-07-26 19:01:57 +0530

    Init Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
+const port = 8080 || process.env.PORT;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+server.listen(port, () => {
+  console.log("Server is liten in port ");
+});
+
+app.get("/", async (req, res) => {
+  res.redirect(`${uuidv4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+    console.log(roomId, userId);
+  });
+});
